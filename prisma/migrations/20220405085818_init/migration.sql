19e2d5ab025f7b1c12234c2d4a10336ea7cea036
TnIoP <pointblank94@naver.com>
2022-04-05 18:03:14 +0900

    - set prisma

---
+++ prisma/migrations/20220405085818_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `Book` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `author` VARCHAR(191) NOT NULL,
+    `publishYear` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
