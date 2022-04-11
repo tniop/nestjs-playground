51a39aa865c594f11e769ad55afe087d99a3cbad
TnIoP <pointblank94@naver.com>
2022-04-11 15:08:48 +0900

    - fix cascade

---
+++ prisma/migrations/20220411054426_test_cascading/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Made the column `authorId` on table `Post` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE `Post` DROP FOREIGN KEY `Post_authorId_fkey`;
+
+-- AlterTable
+ALTER TABLE `Post` MODIFY `authorId` INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
